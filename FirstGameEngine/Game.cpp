1340ee29b916e10964c96d0c083a5cdbb39a90a4
belikovdev <belikovdev99@gmail.com>
2021-06-21 03:34:54 +0300

    Draw triangle

--- FirstGameEngine/Game.cpp
+++ FirstGameEngine/Game.cpp
@@ -25,12 +25,27 @@ void Game::init(const char* title, int xpos, int ypos, int width, int height, bo
 			std::cout << "Window created." << std::endl;
 		}
 
-		renderer = SDL_CreateRenderer(window, -1, 0);
+		SDL_GLContext glContext = SDL_GL_CreateContext(window);
+
+		if (glContext) {
+			std::cout << "GL context created." << std::endl;
+		}
+
+		GLenum error = glewInit();
+		if (error == GLEW_OK) {
+			std::cout << "Glew initialized." << std::endl;
+		}
+
+		SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+		glClearColor(1.0f, 1.0f, 0.0f, 1.0f);
+
+		/*renderer = SDL_CreateRenderer(window, -1, 0);
 
 		if (renderer) {
 			SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
 			std::cout << "Renderer created." << std::endl;
-		}
+		}*/
 
 		isRunning = true;
 	}
@@ -59,9 +74,26 @@ void Game::update() {
 }
 
 void Game::render() {
-	SDL_RenderClear(renderer);
+	glClearDepth(1.0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	SDL_RenderPresent(renderer);
+	glEnableClientState(GL_COLOR_ARRAY);
+	glBegin(GL_TRIANGLES);
+	glColor3f(1.0f, 0.5f, 0.3f);
+	glVertex2f(0, 0);
+	glVertex2f(0, 500);
+	glVertex2f(500, 500);
+
+
+
+
+	glEnd();
+
+	SDL_GL_SwapWindow(window);
+
+	//SDL_RenderClear(renderer);
+
+	//SDL_RenderPresent(renderer);
 }
 
 void Game::clean() {
